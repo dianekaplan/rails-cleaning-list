4171cfa4a3587ea79a42a637697c5166671d0afc
Diane Kaplan <dianekaplan@gmail.com>
2025-12-11 15:15:23 -0500

    only show 'marked done' for 3 seconds

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Auto-dismiss flash messages after 3 seconds
+document.addEventListener('DOMContentLoaded', () => {
+  const flashMessages = document.querySelectorAll('.flash');
+
+  flashMessages.forEach((flash) => {
+    setTimeout(() => {
+      flash.style.transition = 'opacity 0.5s ease-out';
+      flash.style.opacity = '0';
+
+      setTimeout(() => {
+        flash.remove();
+      }, 500);
+    }, 3000);
+  });
+});
